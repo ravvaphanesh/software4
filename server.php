4944bcd06688bb282d2a7de104c114b76187f53b
phanesh ravva <getphanesh@gmail.com>
2018-04-17 14:22:28 +0530

    Update server.php

--- server.php
+++ server.php
@@ -8,7 +8,7 @@
 	$_SESSION['success'] = "";
 
 	// connect to database
-	$db = mysqli_connect('13.127.242.124', 'root', 'phanesh1998', 'registration');
+	$db = mysqli_connect('localhost', 'root', 'phanesh1998', 'registration');
 
 	// REGISTER USER
 	if (isset($_POST['reg_user'])) {
